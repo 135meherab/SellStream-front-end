241eaa09bbc1510dc7f1d972d5a77512f13874e8
meherab <hunterkali66@gmail.com>
2024-05-26 23:10:10 +0600

    [Removed] everything

--- src/Components/Categories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Categories() {
-  return (
-    <div className='text-2xl font-bold text-red-500 text-center'>Category section we are working on it...</div>
-  )
-}
-
-export default Categories
--- src/Components/Reports.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Reports() {
-  return (
-    <div className='text-2xl font-bold text-red-500 text-center'> Reports section we are working on it...</div>
-  )
-}
-
-export default Reports
